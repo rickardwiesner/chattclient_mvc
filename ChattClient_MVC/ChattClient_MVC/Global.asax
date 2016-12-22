eb31345e88bd612f5820a107f8b38e4af894860c
Rickard Wiesner <rickard.wiesner@hotmail.com>
2016-12-22 15:05:20 +0100

    First commit, project created.

---
+++ ChattClient_MVC/ChattClient_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChattClient_MVC.MvcApplication" Language="C#" %>
---
+++ ChattClient_MVC/ChattClient_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChattClient_MVC.Startup))]
+namespace ChattClient_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
